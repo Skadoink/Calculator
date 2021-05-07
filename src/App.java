129b446a4036718deb585096c0e270fa52a8a1ff
Oskar <oskaege@gmail.com>
2021-05-07 23:03:12 +1200

    added frame

--- src/App.java
+++ src/App.java
@@ -1,8 +1,22 @@
 import java.util.Scanner;  // Import the Scanner class
 import java.lang.Math;
+import javax.swing.*;
+import java.awt.FlowLayout;
 
 public class App {
     public static void main(String[] args){
-        System.out.println(1.52321312321312312312316 / 1.297137630);
+        JFrame frame = new JFrame("JFrame test!");
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        JLabel label = new JLabel("JFrame example!!!");
+        JButton button = new JButton();  
+        button.setText("Button");  
+        panel.add(label);  
+        panel.add(button);  
+        frame.add(panel);
+        frame.setSize(300, 300);
+        frame.setLocationRelativeTo(null);  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
