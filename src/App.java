989a10d71b69df13a4604edda86521c784a4af82
Oskar <oskaege@gmail.com>
2021-05-07 22:44:30 +1200

    Started project

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;  // Import the Scanner class
+import java.lang.Math;
+
+public class App {
+    public static void main(String[] args){
+        System.out.println(1.52321312321312312312316 / 1.297137630);
+    }
+}
