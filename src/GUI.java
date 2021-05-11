925ecc1c56f8d15d0b2948c3ad374c0e116a3499
Oskar <oskaege@gmail.com>
2021-05-11 16:44:40 +1200

    Added text box without conditions

--- src/GUI.java
+++ src/GUI.java
@@ -22,6 +22,7 @@ public class GUI extends JFrame {
         for(Integer i=1; i < 10; i++){  //add numbers
             JButton digit = new JButton();  
             digit.setText(i.toString()); 
+            digit.setPreferredSize(new Dimension(40, 50));
             numPanel.add(digit);
         }
         gbc.fill = GridBagConstraints.BOTH;
@@ -29,34 +30,35 @@ public class GUI extends JFrame {
         gbc.gridy = 0;
         gbc.weighty = 0.5;
         gbc.weightx = 0.5;
-        gbc.gridheight = 3; //can't make it work at gridheight = 3
+        gbc.gridheight = 3; 
         gbc.gridwidth = 3;
+        gbc.ipady = 10;
         newFrame.add(numPanel, gbc);  
 
-        JButton zero = new JButton(); //add 0
-        zero.setText("0");
+        JButton zero = new JButton("0"); //add 0
+        zero.setPreferredSize(new Dimension(30, 12));
         gbc.gridx = 0;
         gbc.gridy = 3;
-        // gbc.weighty = 0.5;
         gbc.weightx = 0.5;
-        gbc.gridwidth = 1; //often too big
+        gbc.gridwidth = 1; 
         gbc.gridheight = 1;
+        gbc.insets = new Insets(1, 1, 1, 1);
         newFrame.add(zero, gbc);
 
         JButton execute = new JButton(); //add exe
         execute.setText("EXE");
+        execute.setPreferredSize(new Dimension(60, 12));
         gbc.gridx = 1;
         gbc.gridy = 3;
-        // gbc.weighty = 0.5;
         gbc.weightx = 1;
-        gbc.gridwidth = 1;  //can't get proportional sizing right atm.
+        gbc.gridwidth = 1; 
         gbc.gridheight = 1;
         newFrame.add(execute, gbc);
 
         ArrayList<String>operatorList = new ArrayList<String>(); //add operators
-        Collections.addAll(operatorList, "+", "-", "*", "/"); 
+        Collections.addAll(operatorList, "+", "-", "*", "/", "AC", "C", "+/-"); 
         JPanel oppPanel = new JPanel();
-        oppPanel.setLayout(new GridLayout(1, 4, 2, 2));
+        oppPanel.setLayout(new GridLayout(2, 4, 2, 2));
         for(Integer i = 0; i < operatorList.size(); i++){
             JButton operator = new JButton();
             operator.setText(operatorList.get(i));
@@ -65,23 +67,27 @@ public class GUI extends JFrame {
         gbc.fill = GridBagConstraints.BOTH;
         gbc.gridx = 0;
         gbc.gridy = 4;
-        gbc.ipady = 20;
-        // gbc.weighty = 0.5;
         gbc.gridwidth = 3;
         gbc.gridheight = 1;
         newFrame.add(oppPanel, gbc);
 
-        JPanel labPanel = new JPanel();
-        JLabel label = new JLabel("JLabel example!!!"); //test label
-        labPanel.add(label);
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.gridx = 0;
-        gbc.gridy = 5;
-        // gbc.weighty = 0.5;
-        // gbc.gridwidth = 5;
-        gbc.gridheight = 1;
-        newFrame.add(labPanel, gbc);
+        // JPanel labPanel = new JPanel();
+        // JLabel label = new JLabel("JLabel example!!!"); //test label
+        // labPanel.add(label);
+        // gbc.fill = GridBagConstraints.BOTH;
+        // gbc.gridx = 0;
+        // gbc.gridy = 5;
+        // gbc.gridheight = 1;
+        // newFrame.add(labPanel, gbc);
 
+        JFormattedTextField display = new JFormattedTextField();
+        display.setPreferredSize(new Dimension(100, 10));
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridy = 6;
+        gbc.gridx = 0;
+        gbc.gridwidth = 3;
+        gbc.gridheight = 1;
+        newFrame.add(display, gbc);
 
     }
 }
