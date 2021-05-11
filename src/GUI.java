f3eb936c2d1b31c24c3feb73eb807e958217b45d
Oskar <oskaege@gmail.com>
2021-05-11 21:11:09 +1200

    Formatted the text field to accept valid inputs

--- src/GUI.java
+++ src/GUI.java
@@ -3,14 +3,17 @@ import java.awt.*;
 import java.util.Collections;
 import java.util.Scanner;  // Import the Scanner class
 import java.lang.Math;
+import java.text.ParseException;
+
 import javax.swing.*;
+import javax.swing.text.MaskFormatter;
 
 public class GUI extends JFrame {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws ParseException{
         JFrame newFrame = new JFrame("Calculator with gridbag!"); //made window
         newFrame.setLayout(new GridBagLayout());
-        newFrame.setSize(350, 500);
+        // newFrame.setSize(350, 500);
         newFrame.setLocationRelativeTo(null);  
         newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         newFrame.setVisible(true);
@@ -22,7 +25,7 @@ public class GUI extends JFrame {
         for(Integer i=1; i < 10; i++){  //add numbers
             JButton digit = new JButton();  
             digit.setText(i.toString()); 
-            digit.setPreferredSize(new Dimension(40, 50));
+            digit.setPreferredSize(new Dimension(70, 70));
             numPanel.add(digit);
         }
         gbc.fill = GridBagConstraints.BOTH;
@@ -36,7 +39,7 @@ public class GUI extends JFrame {
         newFrame.add(numPanel, gbc);  
 
         JButton zero = new JButton("0"); //add 0
-        zero.setPreferredSize(new Dimension(30, 12));
+        zero.setPreferredSize(new Dimension(70, 70));
         gbc.gridx = 0;
         gbc.gridy = 3;
         gbc.weightx = 0.5;
@@ -47,7 +50,7 @@ public class GUI extends JFrame {
 
         JButton execute = new JButton(); //add exe
         execute.setText("EXE");
-        execute.setPreferredSize(new Dimension(60, 12));
+        execute.setPreferredSize(new Dimension(140, 70));
         gbc.gridx = 1;
         gbc.gridy = 3;
         gbc.weightx = 1;
@@ -56,12 +59,13 @@ public class GUI extends JFrame {
         newFrame.add(execute, gbc);
 
         ArrayList<String>operatorList = new ArrayList<String>(); //add operators
-        Collections.addAll(operatorList, "+", "-", "*", "/", "AC", "C", "+/-"); 
+        Collections.addAll(operatorList, "+", "-", "C", "AC", "*", "/", "+/-"); 
         JPanel oppPanel = new JPanel();
         oppPanel.setLayout(new GridLayout(2, 4, 2, 2));
         for(Integer i = 0; i < operatorList.size(); i++){
             JButton operator = new JButton();
             operator.setText(operatorList.get(i));
+            operator.setPreferredSize(new Dimension(52, 40));
             oppPanel.add(operator);
         }
         gbc.fill = GridBagConstraints.BOTH;
@@ -71,17 +75,19 @@ public class GUI extends JFrame {
         gbc.gridheight = 1;
         newFrame.add(oppPanel, gbc);
 
-        // JPanel labPanel = new JPanel();
-        // JLabel label = new JLabel("JLabel example!!!"); //test label
-        // labPanel.add(label);
-        // gbc.fill = GridBagConstraints.BOTH;
-        // gbc.gridx = 0;
-        // gbc.gridy = 5;
-        // gbc.gridheight = 1;
-        // newFrame.add(labPanel, gbc);
+        MaskFormatter mask = null;
+        try{
+            mask = new MaskFormatter("****************"); //set up formatiing for text field, 16 chars max
+            mask.setValidCharacters("1234567890+-*/");
+            mask.setPlaceholder("test wow");
+        }
+        catch (ParseException e){
+            e.printStackTrace();
+        }
+        
 
-        JFormattedTextField display = new JFormattedTextField();
-        display.setPreferredSize(new Dimension(100, 10));
+        JFormattedTextField display = new JFormattedTextField(mask); //make formatted text field
+        display.setPreferredSize(new Dimension(210, 20));
         gbc.fill = GridBagConstraints.BOTH;
         gbc.gridy = 6;
         gbc.gridx = 0;
@@ -89,5 +95,6 @@ public class GUI extends JFrame {
         gbc.gridheight = 1;
         newFrame.add(display, gbc);
 
+        newFrame.pack();
     }
 }
